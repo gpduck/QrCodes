af26d50463ee486a9be7240b926813d0f3e543d2
Chris Duck <gpduck@whatsupduck.net>
2015-09-03 23:12:45 -0500

    Dynamically pull ZXing.Net library

--- Default.ps1
+++ Default.ps1
@@ -20,10 +20,10 @@ Properties {
 	}
 }
 
-Task default -Depends Pack
+Task default -Depends Clean,Pack
+Task restore -Depends Clean,PackageRestore
 
-Task Pack {
-	ipmo NuGet
+Task Pack -Depends GetNuget,PackageRestore {
 	Assert (Test-Path $NuSpecFile) -FailureMessage "$NuSpecFile does not exist"
 	if(!(Test-Path $TargetDir)) {
 		mkdir $TargetDir > $null
@@ -33,4 +33,22 @@ Task Pack {
 	$VersionDate = [int][Datetime]::Now.ToString("yyyyMMdd")
 	$OutputVersion = New-Object System.Version($NuSpecVersion.Major, $NuSpecVersion.Minor, $VersionDate, $BuildNumber)
 	exec { nuget pack $NuSpecFile -OutputDirectory $TargetDir -BasePath $BasePath -NoPackageAnalysis -NonInteractive -Version ($OutputVersion.ToString())}
+}
+
+Task GetNuget {
+	if(!(Test-Path $ProjectDir\nuget.exe)) {
+		invoke-webrequest -uri https://nuget.org/nuget.exe -outfile $ProjectDir\nuget.exe
+	}
+	function script:nuget {
+		&$ProjectDir\nuget.exe $Args
+	}
+}
+
+Task PackageRestore {
+	nuget install -ExcludeVersion -OutputDirectory $BasePath packages.config
+}
+
+Task Clean {
+	rm $BasePath\ZXing.Net -recurse -erroraction SilentlyContinue
+	rm bin -recurse -erroraction SilentlyContinue
 }
