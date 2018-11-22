edca10967bd6162b4e0df92b8a218c12df6155fa
Chris <gpduck@whatsupduck.net>
2018-11-21 22:02:24 -0600

    Update ZXing.net and regenerate psd1 file

---
+++ Tests/QRCodes.tests.ps1
@@ -0,0 +1,24 @@
+$here = Split-Path -Parent $MyInvocation.MyCommand.Path
+$sut = Join-Path $here "../QrCodes"
+
+Import-module $sut -Force
+
+Describe "QRCodes" {
+    It "creates a QR code" {
+        $QRCode = "test string" | ConvertTo-QRCode
+        $QRCode | Should -Not -Be $null
+        $QRCode | Should -BeOfType ZXing.QrCode.Internal.QRCode
+    }   
+}
+
+Describe "Format-QRCode" {
+    It "adds correct padding" {
+        $QRCode = "test string" | ConvertTo-QRCode
+        $String = $QRCode | Format-QRCode
+
+        $Width = $QRCode.Matrix.Width + 4 + [Environment]::NewLine.Length
+        $Height = [Math]::Ceiling($QRCode.Matrix.Height / 2) + 4
+
+        $String.Length | Should -Be ($Width * $Height)
+    }
+}
